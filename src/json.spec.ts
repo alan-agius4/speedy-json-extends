c23872156d35961fad59c847ac0088fe01a1cae9
Alan Agius <alan.agius4@gmail.com>
2017-04-01 21:39:39 +0200

    test(json): add test for same level extends

--- src/json.spec.ts
+++ src/json.spec.ts
@@ -47,11 +47,33 @@ describe("jsonSpec", () => {
 			}
 		};
 
+		const THIRD_EXTEND_FILE = {
+			rules: {
+				message: {
+					minLength: 40
+				}
+			}
+		};
+
+		const MULTIPLE_EXTEND_FILE = {
+			extends: [
+				"./config/secondary-extend.json",
+				"./config/third-extend.json"
+			],
+			rules: {
+				message: {
+					noWhiteSpace: true
+				}
+			}
+		};
+
 		beforeEach(() => {
 			mockFs({
 				"speedy-commit-msg-hook.json": JSON.stringify(MAIN_FILE),
 				"speedy-commit-msg-hook-alias.json": JSON.stringify(MAIN_FILE_WITH_ALIAS),
+				"multiple-extends.json": JSON.stringify(MULTIPLE_EXTEND_FILE),
 				config: {
+					"third-extend.json": JSON.stringify(THIRD_EXTEND_FILE),
 					"primary-extend.json": JSON.stringify(PRIMARY_EXTEND_FILE),
 					"secondary-extend.json": JSON.stringify(SECONDARY_EXTEND_FILE)
 				}
@@ -62,8 +84,14 @@ describe("jsonSpec", () => {
 			mockFs.restore();
 		});
 
+		it("should merge values of extended files ordered by per position in array", async done => {
+			const result = await json.read<any>("multiple-extends.json");
+			expect(result.rules.message.minLength).toBe(40);
+			expect(result.rules.message.noWhiteSpace).toBe(true);
+			done();
+		});
 
-		it("should merge object of extended config with alias", async done => {
+		it("should merge object with named extends", async done => {
 			const result = await json.read<any>("speedy-commit-msg-hook-alias.json", {
 				"primary-extend": "./config/primary-extend.json"
 			});
@@ -72,7 +100,7 @@ describe("jsonSpec", () => {
 			done();
 		});
 
-		it("should merge object of extended config", async done => {
+		it("should merge object of extended file", async done => {
 			const result = await json.read<any>("speedy-commit-msg-hook.json");
 			expect(result.rules.message.maxLength).toBe(100);
 			expect(result.rules.message.noDashes).toBe(true);
@@ -85,7 +113,7 @@ describe("jsonSpec", () => {
 			done();
 		});
 
-		it("should merge properties of nested extended config", async done => {
+		it("should merge properties of nested extended file", async done => {
 			const result = await json.read<any>("speedy-commit-msg-hook.json");
 			expect(result.rules.message.noDashes).toBe(true);
 			expect(result.rules.message.minLength).toBe(2);
